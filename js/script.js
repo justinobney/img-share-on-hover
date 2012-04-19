db6460cb920e7daef18b53e84ff3126ad77a13cd
Justin Obney <justinobney@gmail.com>
2012-04-19 08:58:23 -0500

    Dynamic Image URLs
    Gets Image URLs from a.href || img.src
    shortens each URL

--- js/script.js
+++ js/script.js
@@ -1,21 +1,10 @@
-var test_options = {
-    tw_user: 'thepostmonkey',
-    post_url: 'http://thepostmonkey.com/',
-    post_img: 'http://thepostmonkey.com/wp-content/uploads/2012/04/rag-23-Copy1.jpg',
-    post_title: 'The PostMonkey Strikes Again',
-    post_desc: 'More Monkey Antics at http://thePostMonkey.com',
-    bit_user: 'jay3917',
-    bit_key: 'R_67a7af8c9a6bb9ee62920b61687b0857'
-}
-
-
-function tweetButton(url) {
+function tweetButton(url, tw_user, title) {
 
     // Create the twitter URL
-    var tweeturl = 'http://twitter.com/share?url=' + encodeURI(url) + '&via=' + test_options.tw_user  + '&text=' + test_options.post_title;
+    var tweeturl = 'http://twitter.com/share?url=' + encodeURI(url) + '&via=' + tw_user  + '&text=' + title;
 
     // Place the text on the page. Change body to wherever you want the button placed. 
-    return '<a title="' + test_options.post_title + '" href=" ' + tweeturl + ' " target="_blank"><img src="http://www.likebuttonshirt.com/img/tweet-button-small.png" alt="Share on Twitter" /></a>';
+    return '<a title="' + title + '" href=" ' + tweeturl + ' " target="_blank"><img src="http://www.likebuttonshirt.com/img/tweet-button-small.png" alt="Share on Twitter" /></a>';
 };
 
 function faceButton(url, imgURL, title, summary) {
@@ -27,6 +16,20 @@ function faceButton(url, imgURL, title, summary) {
 }
 
 function getShare(el) {
+    var test_options = {
+        tw_user: 'thepostmonkey',
+        post_url: 'http://thepostmonkey.com/',
+        post_img: 'http://thepostmonkey.com/wp-content/uploads/2012/04/rag-23-Copy1.jpg',
+        post_title: 'The PostMonkey Strikes Again',
+        post_desc: 'More Monkey Antics at http://thePostMonkey.com',
+        bit_user: 'jay3917',
+        bit_key: 'R_67a7af8c9a6bb9ee62920b61687b0857'
+    }
+    
+    var ext = el.href.split("?")[0].split(".").reverse()[0].toString();
+    
+    test_options.post_img = ("jpg|png|gif".indexOf(ext) > -1) ? el.href : $(el).find('img').attr('src');
+    
     get_short_url(test_options.post_img, test_options.bit_user, test_options.bit_key, function(resp){
         
         if(resp.status_code === 200) {
@@ -35,7 +38,7 @@ function getShare(el) {
         
         $(el).hover(function () {
             $(el).block({
-                message: tweetButton(test_options.post_img) + '<br />' + faceButton(test_options.post_img, test_options.post_img, test_options.post_title, test_options.post_desc) + '<br /><br />'
+                message: tweetButton(test_options.post_img, test_options.tw_user, test_options.post_title) + '<br />' + faceButton(test_options.post_img, test_options.post_img, test_options.post_title, test_options.post_desc) + '<br /><br />'
             });
         }, function () {
             $(el).unblock();
