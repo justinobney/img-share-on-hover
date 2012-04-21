da5818bc642c8b238b6a27b202e18809189ff205
Justin Obney <justinobney@gmail.com>
2012-04-21 09:21:05 -0500

    Almost DONE!!!

--- js/sharer.js
+++ js/sharer.js
@@ -45,7 +45,10 @@ var _sharer = _sharer || {};
         });
 
         getPopular();
+        
+        this.getShare();
     }
+    
     function getPopular() {
         var popular, popular_url = 'monkey.html';
         //'http://thepostmonkey.com/'
@@ -67,13 +70,15 @@ var _sharer = _sharer || {};
         var tweeturl = 'http://twitter.com/share?url=' + encodeURI(url) + '&via=' + tw_user + '&text=' + title;
 
         // Place the text on the page. Change body to wherever you want the button placed.
-        return '<a title="' + title + '" href=" ' + tweeturl + ' " target="_blank"><img src="http://dl.dropbox.com/u/2857953/PostMonkey/twitter_button.png" alt="Share on Twitter" /></a>';
+        var tw_img_url = "http://cdn1.iconfinder.com/data/icons/Modern_Web_Social_Icons_by_SimekOneLove/128/twitter.png";
+        return '<a title="' + title + '" href=" ' + tweeturl + ' " target="_blank"><img src="' + tw_img_url + '" alt="Share on Twitter" /></a>';
     };
 
     function faceButton(url, imgURL, title, summary) {
         var fb_url_template = 'http://www.facebook.com/sharer.php?s=100&p[url]=' + encodeURI(url) + '&p[images][0]=' + encodeURI(imgURL) + '&p[title]=' + encodeURI(title) + '&p[summary]=' + encodeURI(summary);
-
-        var link_template = '<a title="Post this Monkey on Facebook" href="' + fb_url_template + '" target="_blank"><img src="http://dl.dropbox.com/u/2857953/PostMonkey/facebook_button.png" alt="Share on Facebook" /></a>';
+        
+        var fb_img_url = "http://cdn1.iconfinder.com/data/icons/Modern_Web_Social_Icons_by_SimekOneLove/128/facebook.png";
+        var link_template = '<a title="Post this Monkey on Facebook" href="' + fb_url_template + '" target="_blank"><img src="' + fb_img_url + '" alt="Share on Facebook" /></a>';
 
         return link_template;
     }
@@ -82,36 +87,27 @@ var _sharer = _sharer || {};
         var test_options = {
             tw_user : 'thepostmonkey',
             post_url : 'http://thepostmonkey.com/',
-            post_img : 'http://thepostmonkey.com/wp-content/uploads/2012/04/rag-23-Copy1.jpg',
+            post_img : document.location.href,
             post_title : 'The PostMonkey Strikes Again',
             post_desc : 'More Monkey Antics at http://thePostMonkey.com',
             bit_user : 'jay3917',
             bit_key : 'R_67a7af8c9a6bb9ee62920b61687b0857'
         }
 
-        var ext = el.href.split("?")[0].split(".").reverse()[0].toString();
-
-        test_options.post_img = ("jpg|png|gif".indexOf(ext) > -1) ? el.href : $(el).find('img').attr('src');
-
-        var href = encodeURIComponent(test_options.post_img), title = encodeURIComponent(document.title);
-
-        var share_url = '<a href="sharer.html?img=' + href + '&title=' + title + '"';
-
-        get_short_url(share_url, test_options.bit_user, test_options.bit_key, function(resp) {
+        get_short_url(test_options.post_img, test_options.bit_user, test_options.bit_key, function(resp) {
 
             if(resp.status_code === 200) {
                 test_options.post_img = resp.data.url;
             }
 
-            var share_buttons = tweetButton(share_url, test_options.tw_user, test_options.post_title) + '&nbsp;' + faceButton(test_options.post_img, test_options.post_img, test_options.post_title, test_options.post_desc);
+            var share_buttons = faceButton(test_options.post_img, test_options.post_img, test_options.post_title, test_options.post_desc) + '&nbsp;' + tweetButton(test_options.post_img, test_options.tw_user, test_options.post_title);
+            $('#share_containter').append(share_buttons);
 
-            $(el).parent().parent().prepend(share_buttons);
+            $('#short_link').val(test_options.post_img);
 
         });
-
-        $(el).parent().parent().prepend(share_buttons);
     }
-
+   
     function get_short_url(long_url, login, api_key, func) {
         $.getJSON("http://api.bitly.com/v3/shorten?callback=?", {
             "format" : "json",
